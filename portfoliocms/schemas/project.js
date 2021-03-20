43ce9b73005422ce82e267e295b804ff48e3f47c
Milton Chung <miltonjchung@gmail.com>
2021-03-20 15:39:02 -0700

    sanity schema

---
+++ portfoliocms/schemas/project.js
@@ -0,0 +1,49 @@
+export default {
+	name: "projects",
+	title: "Projects",
+	type: "document",
+	fields: [
+		{
+			name: "title",
+			title: "Title",
+			type: "string",
+		},
+		{
+			name: "projectPicture",
+			title: "Project Picture",
+			type: "image",
+			options: { hotspot: true },
+		},
+		{
+			name: "skills",
+			title: "Skills",
+			type: "array",
+			of: [{ type: "string" }],
+		},
+		{
+			name: "description",
+			title: "Description",
+			type: "blockContent",
+		},
+		{
+			name: "githubLink",
+			title: "Github Link",
+			type: "string",
+		},
+		{
+			name: "liveLink",
+			title: "Live Link",
+			type: "string",
+		},
+		{
+			name: "featured",
+			title: "Featured",
+			type: "boolean",
+		},
+		{
+			name: "order",
+			title: "Order",
+			type: "number",
+		},
+	],
+};
