de204599a6fa3bd356e124434c7cfaaad0779c97
Milton Chung <miltonjchung@gmail.com>
2023-02-14 23:50:13 -0800

    configure redirects

--- next.config.js
+++ next.config.js
@@ -16,5 +16,14 @@ module.exports = {
     EMAILJS_SERVICE_ID: process.env.EMAILJS_SERVICE_ID,
     EMAILJS_TEMPLATE_ID: process.env.EMAILJS_TEMPLATE_ID,
     EMAILJS_PUBLIC_KEY: process.env.EMAILJS_PUBLIC_KEY
+  },
+  async redirects() {
+    return [
+      {
+        source: '/*',
+        destination: '/',
+        permanent: true
+      }
+    ];
   }
 };
