23a0aa676d435ef3001df6c18668a5f5ee0f540f
Milton Chung <miltonjchung@gmail.com>
2023-06-12 15:25:34 -0700

    tarlte eaid test

---
+++ pages/tartletest/index.tsx
@@ -0,0 +1,10 @@
+const TartleTest = () => {
+  return (
+    <div className="tartle-test">
+      <h1>Tartle EAID:</h1>
+      <input type="text" id="_tartle_eaid" value="" placeholder="Tartle EAID" />
+    </div>
+  );
+};
+
+export default TartleTest;
