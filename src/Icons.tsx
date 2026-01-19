c247b1864bff557e667942544706b803652d4de3
Milton Chung <miltonjchung@gmail.com>
2026-01-18 18:45:22 -0800

    clear unused svg

--- src/Icons.tsx
+++ src/Icons.tsx
@@ -7,8 +7,6 @@ import CopyIcon from '../public/assets/svg/copy.svg';
 // Logos
 import GithubIcon from '../public/assets/logos/github.svg';
 import LinkedinIcon from '../public/assets/logos/linkedin.svg';
-import NpmLogo from '../public/assets/logos/npm.svg';
-import SqlLogo from '../public/assets/logos/sql.svg';
 import CssLogo from '../public/assets/logos/css.svg';
 import HtmlLogo from '../public/assets/logos/html.svg';
 import SassLogo from '../public/assets/logos/sass.svg';
@@ -16,18 +14,13 @@ import FigmaLogo from '../public/assets/logos/figma.svg';
 import NodejsLogo from '../public/assets/logos/nodejs.svg';
 import PythonLogo from '../public/assets/logos/python.svg';
 import SanityLogo from '../public/assets/logos/sanity.svg';
-import VSCodeLogo from '../public/assets/logos/vscode.svg';
 import ReactLogo from '../public/assets/logos/react.svg';
-import ExpressLogo from '../public/assets/logos/express.svg';
-import MongoDbLogo from '../public/assets/logos/mongodb.svg';
 import PostmanLogo from '../public/assets/logos/postman.svg';
 import JavascriptLogo from '../public/assets/logos/javascript.svg';
 import TypescriptLogo from '../public/assets/logos/typescript.svg';
 import FrontendMentorLogo from '../public/assets/logos/frontendmentor.png';
 import EdabitLogo from '../public/assets/logos/edabit.png';
 import ExercismLogo from '../public/assets/logos/exercism.png';
-import HackerRankLogo from '../public/assets/logos/hackerrank.svg';
-import CodepenLogo from '../public/assets/logos/codepen.svg';
 import ConvexLogo from '../public/assets/logos/convex.svg';
 import NextjsLogo from '../public/assets/logos/nextjs.svg';
 import RubyLogo from '../public/assets/logos/ruby.svg';
@@ -53,8 +46,6 @@ export {
   CopyIcon,
   GithubIcon,
   LinkedinIcon,
-  NpmLogo,
-  SqlLogo,
   CssLogo,
   HtmlLogo,
   SassLogo,
@@ -62,18 +53,13 @@ export {
   NodejsLogo,
   PythonLogo,
   SanityLogo,
-  VSCodeLogo,
   ReactLogo,
-  ExpressLogo,
-  MongoDbLogo,
   PostmanLogo,
   JavascriptLogo,
   TypescriptLogo,
   FrontendMentorLogo,
   EdabitLogo,
   ExercismLogo,
-  HackerRankLogo,
-  CodepenLogo,
   ConvexLogo,
   NextjsLogo,
   RubyLogo,
